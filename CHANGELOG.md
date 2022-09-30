ea4179517bff7863fbb993dd854b81969b100a10
rizzzse <rizzzse@gmail.com>
2022-09-30 20:08:27 +0900

    add xxxInXxx constants and functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,12 @@
 - `DateTime` class
 - `isLeapYear` function
 - `leapDays` function
+- `monthsInYear` constant
+- `daysInYear` constant
+- `DaysInMonth` type
+- `daysInMonth` function
+- `daysInWeek` constant
+- `hoursInDay` constant
+- `minutesInHour` constant
+- `secondsInMinute` constant
+- `millisInSecond` constant
--- src/datetime.ts
+++ src/datetime.ts
@@ -32,3 +32,21 @@ export const isLeapYear = (year: number): boolean => {
 export const leapDays = (year: number): number => {
     return (year / 4 | 0) - (year / 100 | 0) + (year / 400 | 0);
 };
+
+export type DaysInMonth = 28 | 29 | 30 | 31;
+
+const daysInMonthArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] as const;
+
+export const daysInMonth = (year: number, month: number): DaysInMonth => {
+    const leapDay = +(month === 2 && isLeapYear(year));
+    return daysInMonthArray[month - 1] + leapDay as DaysInMonth;
+};
+
+export const daysInYear = 365;
+export const daysInWeek = 7;
+export const monthsInYear = 12;
+
+export const hoursInDay = 24;
+export const minutesInHour = 60;
+export const secondsInMinute = 60;
+export const millisInSecond = 1000;
