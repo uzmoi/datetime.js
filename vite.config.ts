ef4ac13d6336c50817360ca235527c252dd39606
rizzzse <rizzzse@gmail.com>
2022-10-06 19:27:03 +0900

    set dependencies as external

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import pkg from "./package.json";
 
 export default defineConfig({
     build: {
@@ -7,6 +8,8 @@ export default defineConfig({
             formats: ["es"],
             fileName: "datetime"
         },
+        rollupOptions: {
+            external: Object.keys(pkg.dependencies),
+        },
     },
-    test: {},
 });
