ae1ee0a63f83d05a639d4b1dc762e019a3d589a7
rizzzse <rizzzse@gmail.com>
2023-03-11 20:39:04 +0900

    add test for dateToString and timeToString

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,9 +1,31 @@
 import { describe, expect, test } from "vitest";
-import { offsetToString } from "./string";
+import type { DateObject, TimeObject } from "./datetime";
+import { dateToString, offsetToString, timeToString } from "./string";
 
-describe.todo("dateToString");
+describe("dateToString", () => {
+    test.todo("basic");
+    test("extended", () => {
+        const date: DateObject = {
+            year: 95,
+            month: 8,
+            day: 4,
+        };
+        expect(dateToString(date)).toBe("0095-08-04");
+    });
+});
 
-describe.todo("timeToString");
+describe("timeToString", () => {
+    test.todo("basic");
+    test("extended", () => {
+        const time: TimeObject = {
+            hour: 16,
+            minute: 8,
+            second: 4,
+            millisecond: 2,
+        };
+        expect(timeToString(time)).toBe("16:08:04.002");
+    });
+});
 
 describe("offsetToString", () => {
     test("Z", () => {
