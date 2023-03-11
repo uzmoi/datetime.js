48a21b1aa1e38183986e35e127b961fc9d9a0468
rizzzse <rizzzse@gmail.com>
2023-03-11 23:14:05 +0900

    move test

--- src/datetime.test.ts
+++ src/datetime.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { DateTime } from "./datetime";
-import { dayOfYear, isLeapYear, weekday, weeksInYear } from "./number";
+import { isLeapYear } from "./number";
 
 describe("DateTime", () => {
     test(".toString()", () => {
@@ -49,39 +49,3 @@ describe("DateTime", () => {
         expect(base.endOf(key)).toStrictEqual(DateTime.from(expected));
     });
 });
-
-test.each([
-    { year: 2017, leap: false, weekDay: 0 },
-    { year: 2018, leap: false, weekDay: 1 },
-    { year: 2022, leap: false, weekDay: 6 },
-    { year: 2012, leap: true, weekDay: 0 },
-    { year: 2024, leap: true, weekDay: 1 },
-    { year: 2028, leap: true, weekDay: 6 },
-])(
-    "weeksInYear($year) // leap=$leap, weekday=$weekday",
-    ({ year, leap, weekDay }) => {
-        expect(isLeapYear(year)).toBe(leap);
-        expect(weekday({ year, month: 1, day: 1 })).toBe(weekDay);
-        let expectWeeksInYear = 0;
-        for (
-            let x = DateTime.from([year]);
-            x.year === year;
-            x = x.plus({ days: 1 })
-        ) {
-            if (weekday(x) === 0) {
-                expectWeeksInYear++;
-            }
-        }
-        expect(weeksInYear(year)).toBe(expectWeeksInYear);
-    },
-);
-
-test.each(Array.from({ length: 12 }, (_, i) => DateTime.from([2022, i + 1])))(
-    "yearday('%s')",
-    dt => {
-        expect(dayOfYear(dt)).toBe(
-            Math.floor((+dt - +dt.startOf("year")) / 86400000),
-        );
-        expect(dt.startOf("year").plus({ days: dayOfYear(dt) })).toEqual(dt);
-    },
-);
