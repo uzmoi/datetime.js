5f0d560069bd9c929f4a9baea09a1bc9374a32ef
rizzzse <rizzzse@gmail.com>
2023-03-11 19:40:08 +0900

    rename entry

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     build: {
         lib: {
-            entry: "src/main.ts",
+            entry: "src/index.ts",
             fileName: "datetime",
             name: "rdt",
         },
