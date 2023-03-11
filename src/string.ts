d1be0d88d193ba83fea3a5d9a4ccfb2be19f6142
rizzzse <rizzzse@gmail.com>
2023-03-11 15:01:37 +0900

    rename formatInt

--- src/string.ts
+++ src/string.ts
@@ -1,21 +1,25 @@
 import { DateObject, TimeObject } from "./datetime";
 
-const pad0 = (n: number, len: number) => String(n | 0).padStart(len, "0");
+const formatInt = (n: number, len: number) => String(n | 0).padStart(len, "0");
 
 export const dateToString = (date: DateObject): string => {
     return (
-        pad0(date.year, 4) + "-" + pad0(date.month, 2) + "-" + pad0(date.day, 2)
+        formatInt(date.year, 4) +
+        "-" +
+        formatInt(date.month, 2) +
+        "-" +
+        formatInt(date.day, 2)
     );
 };
 
 export const timeToString = (time: TimeObject): string => {
     return (
-        pad0(time.hour, 2) +
+        formatInt(time.hour, 2) +
         ":" +
-        pad0(time.minute, 2) +
+        formatInt(time.minute, 2) +
         ":" +
-        pad0(time.second, 2) +
+        formatInt(time.second, 2) +
         "." +
-        pad0(time.millisecond, 3)
+        formatInt(time.millisecond, 3)
     );
 };
