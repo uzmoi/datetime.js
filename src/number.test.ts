81bcaf848a566eb497e867cb5bd4acfc5dd24c4e
rizzzse <rizzzse@gmail.com>
2023-03-12 16:56:14 +0900

    remove Math.floor

--- src/number.test.ts
+++ src/number.test.ts
@@ -67,9 +67,7 @@ describe("weekOfMonth", () => {
 test.each(Array.from({ length: 12 }, (_, i) => DateTime.from([2022, i + 1])))(
     "dayOfYear('%s')",
     dt => {
-        expect(dayOfYear(dt)).toBe(
-            Math.floor((+dt - +dt.startOf("year")) / 86400000),
-        );
+        expect(dayOfYear(dt)).toBe((+dt - +dt.startOf("year")) / 86400000);
         expect(dt.startOf("year").plus({ days: dayOfYear(dt) })).toEqual(dt);
     },
 );
