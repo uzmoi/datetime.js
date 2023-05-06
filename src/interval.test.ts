7b85887f3787973e2ea5ac638873dbc13307685c
rizzzse <rizzzse@gmail.com>
2023-05-06 18:26:13 +0900

    refactor test

--- src/interval.test.ts
+++ src/interval.test.ts
@@ -33,38 +33,39 @@ describe("Interval", () => {
         // prettier-ignore
         { years: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 0, milliseconds: 0 },
     ])("new %j", dur => {
-        expect(new Interval(start, start.plus(dur))).toMatchObject({ ...dur });
+        expect(Interval.after(start, dur)).toMatchObject({ ...dur });
     });
-    {
+    test(".to('years')", () => {
+        const interval = Interval.after(start, { years: 1 });
+        expect(interval.to("years")).toBe(1);
+    });
+    test(".to('months')", () => {
+        const interval = Interval.after(start, { years: 1, months: 2 });
+        expect(interval.to("months")).toBe(1 * monthsInYear + 2);
+    });
+    test(".to('milliseconds')", () => {
         // prettier-ignore
         const expectedDays = plus.years * daysInYearWithoutLeapDay
-            + daysInMonth(start.year + plus.years, start.month + 1)
-            + daysInMonth(start.year + plus.years + 1, (start.month + 2) % monthsInYear);
-        test.each([
-            ["years" as const, plus.years],
-            ["months" as const, plus.years * monthsInYear + plus.months],
-            // prettier-ignore
-            ["milliseconds" as const, ([
-                [1,               plus.days],
-                [hoursInDay,      plus.hours],
-                [minutesInHour,   plus.minutes],
-                [secondsInMinute, plus.seconds],
-                [millisInSecond,  plus.milliseconds],
-            ] as const).reduce((prev, [rate, value]) => prev * rate + value, expectedDays)],
-            // ["weeks" as const, Math.floor(expectedDays / daysInWeek)],
-        ])(".to('%s')", (key, expected) => {
-            const interval = new Interval(start, start.plus(plus));
-            expect(interval.to(key)).toBe(expected);
-        });
-    }
+                + daysInMonth(start.year + plus.years, start.month + 1)
+                + daysInMonth(start.year + plus.years + 1, (start.month + 2) % monthsInYear)
+                + plus.days;
+        const expected = [
+            [hoursInDay, plus.hours],
+            [minutesInHour, plus.minutes],
+            [secondsInMinute, plus.seconds],
+            [millisInSecond, plus.milliseconds],
+        ].reduce((prev, [rate, value]) => prev * rate + value, expectedDays);
+        const interval = Interval.after(start, plus);
+        expect(interval.to("milliseconds")).toBe(expected);
+    });
     test("(unixEpoch..now).to('milliseconds') === Date.now()", () => {
         const now = Date.now();
         const interval = Interval.from("1970-01-01T00:00:00.000Z", now);
         expect(interval.to("milliseconds")).toBe(now);
     });
     test("overlaps", () => {
-        const a = Interval.from([2000], [2020]);
-        const b = Interval.from([2010], [2030]);
+        const a = Interval.after([2000], { years: 20 });
+        const b = Interval.after([2010], { years: 20 });
         expect(a.overlaps(b)).toBe(true);
         expect(b.overlaps(a)).toBe(true);
     });
